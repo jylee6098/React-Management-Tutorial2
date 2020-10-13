624e1b497f6a7d8c3bd9c533ef5d8ee674c7b7a3
jylee6098 <nakkee@naver.com>
2020-10-13 18:51:39 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer = [{
+  'id': 1,
+  'image': 'http://placeimage.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : '951231',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'http://placeimg.com/64/64/2',
+  'name' : '임꺽정',
+  'birthday' : '951230',
+  'gender' : '남자',
+  'job' : '프로그래머'
+},
+{
+  'id' : 3,
+  'image' : 'http://placeimg.com/64/64/3',
+  'name' : '장보고',
+  'birthday' : '951220',
+  'gender' : '남자',
+  'job' : '디자이너'
+}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React Project!
-        </p>
-       
-      </header>
+    <div>{
+      customer.map(c => {
+        return(
+          <Customer
+          key={c.id}
+          id = {c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}
+          />
+        )
+      })
+      }
     </div>
   );
 }
